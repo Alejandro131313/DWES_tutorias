6b547b7505e7622c1ac9905e7fec132f5342a426
Alejandro131313 <122273941+Alejandro131313@users.noreply.github.com>
2024-02-08 09:03:53 +0100

    Spring Security
    Añadida seguridad

--- DWES_2024_tutorias-main/src/main/resources/data.sql
+++ DWES_2024_tutorias-main/src/main/resources/data.sql
@@ -4,12 +4,16 @@ insert into curso (nombre) values ('2A');
 insert into curso (nombre) values ('2B');
 insert into curso (nombre) values ('3A');
 insert into curso (nombre) values ('3B');
+
+
 insert into plan (nombre) values ('P2023-2024-1A');
 insert into plan (nombre) values ('P2023-2024-1B');
 insert into plan (nombre) values ('P2023-2024-2A');
 insert into plan (nombre) values ('P2023-2024-2B');
 insert into plan (nombre,FK_CURSO) values ('P2023-2024-3A','1A');
 insert into plan (nombre,FK_CURSO) values ('P2023-2024-3B','1B');
+
+
 insert into tutor (nombre,email) values ('Marcos','marcos@educastur.org');
 insert into tutor (nombre,email) values ('Javier','javier@educastur.org');
 insert into tutor (nombre,email,plan) values ('Maria','maria@educastur.org',3);
@@ -26,3 +30,7 @@ insert into actividad (id,obligatoria,nombre,descripcion) values (3,true,'ZAR','
 insert into enmarca (id,plan,actividad,fecha) values (1,1,1,'20-10-2024');
 insert into enmarca (id,plan,actividad,fecha) values (2,2,2,'1-05-2024');
 insert into enmarca (id,plan,actividad,fecha) values (3,3,3,'12-03-2024');
+
+
+insert into Usuario(usuario, password) values ('Maria','$2a$12$5kwoR2jH6WD2WZRb9z1kuuqxCFebaDyFDNCi1NceD3PU798h43ytS');
+insert into Usuario(usuario, password) values ('Pablo','$2a$12$AxI1Xzw4BP43REntQUUOV.W172FXEqn9yNmtsSyMxPjDoEwO.M922');
